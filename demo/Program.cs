f3f16d73bd48948bf684d55644783eba2fd25a59
REKOR\zwalker <zwalker@rekor.ai>
2025-06-20 08:29:11 -0400

    demo

---
+++ demo/Program.cs
@@ -0,0 +1,10 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using ImageMetricsLib;
+
+var check = await SdkLicenseService.InitializeLicensing("demo.bin");
+Console.WriteLine(check);
+ImageMetricsCalc metic = new ImageMetricsCalc();
+//byte[] imageBytes = File.ReadAllBytes("sun.jpg");
+//var test = metic.CalculateClarity(imageBytes);
+//Console.WriteLine(test);
